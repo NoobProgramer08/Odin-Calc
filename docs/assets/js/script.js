2b74b001565a9000df8e3d27d6ac4e421e17711e
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-01-19 23:57:07 +0800

    Follow the standards of odin and migh refactor

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,6 +1,8 @@
 const clearBtn = document.querySelector("#clear");
 const currentStatus = document.querySelector("#input");
 const evaluate = document.querySelector("#evaluate");
+let operatorInstance = 0;
+
 
 clearBtn.addEventListener('click',clearInput);
 evaluate.addEventListener('click',evaluateNumbers);
@@ -9,7 +11,66 @@ evaluate.addEventListener('click',evaluateNumbers);
 function appendNumber(event){
 const target = event.target.textContent;
 currentStatus.value += target;
+    checkOperator();
+}
+
+TODO: //* This part might need refactoring
+function checkOperator(){
+    const currentValues = currentStatus.value.split("");
+    let firstOperator = "";
+    currentValues.forEach((element) => {
+    if(element === "+" || element === "-" || element === "*" || element === "/"){
+        operatorInstance++;
+        if(operatorInstance == 1){
+            firstOperator = element;
+        
+        }
+    }
+});
+
+
     
+    if(operatorInstance >= 2){
+        if(firstOperator === "+"){
+            const splitValue = currentStatus.value.split("+");
+            const num1 = splitValue[0];
+            const num2 = splitValue[1];
+
+            console.log(num1);
+            console.log(num2);
+            add(Number(num1),Number(num2));
+    
+        }else if(firstOperator === "-"){
+            const splitValue = currentStatus.value.split("-");
+            const num1 = splitValue[0];
+            const num2 = splitValue[1];
+            subtract(Number(num1),Number(num2));
+    
+        }else if(firstOperator === "*"){
+            const splitValue = currentStatus.value.split("*");
+            const num1 = splitValue[0];
+            const num2 = splitValue[1];
+            multiply(Number(num1),Number(num2));
+    
+        }else{
+            const splitValue = currentStatus.value.split("/");
+            const num1 = splitValue[0];
+            const num2 = splitValue[1];
+    
+            if(Number(num2) === 0){
+                displayEvaluation("Cannot divide");
+                return;
+    
+            }
+            divide(Number(num1),Number(num2));
+        }
+
+    }
+    operatorInstance = 0;
+    
+
+    
+
 }
 
 function clearInput(){
@@ -18,62 +79,83 @@ function clearInput(){
 
 function evaluateNumbers(){
     const numbers = currentStatus.value;
-    const arrayed = numbers.split("");
-    let operation = "";
-    arrayed.forEach(element => {
-            if(element === "+" || element == "-" || element == "*" || element == "÷"){
-                operation = element;
-                return;
-                
-            }
+    const findOperation = numbers.split("");
+    let operation = "", instance = 0;
+    
+    findOperation.forEach((element) => { 
+        if(element === "+" || element === "-" || element === "*" || element === "/"){
+            operation = element;
+            instance++;
+            return;
+        
+        }
     });
+    if(instance >= 2){
+        instance = 0;
+        return;
+    }
 
     if(operation === "+"){
-        const separated = numbers.split("+");
-        const num1 = separated[0];
-        const num2 = separated[1];
-        add(num1,num2);
+        const split = numbers.split("+");
+        const num1 = split[0];
+        const num2 = split[1];
+        add(Number(num1),Number(num2));
 
     }else if(operation === "-"){
-        const separated = numbers.split("-");
-        const num1 = separated[0];
-        const num2 = separated[1];
-        subtract(num1,num2);
+        const split = numbers.split("-");
+        const num1 = split[0];
+        const num2 = split[1];
+        subtract(Number(num1),Number(num2));
 
     }else if(operation === "*"){
-        const separated = numbers.split("*");
-        const num1 = separated[0];
-        const num2 = separated[1];
-        multiply(num1,num2);
-
+        const split = numbers.split("*");
+        const num1 = split[0];
+        const num2 = split[1];
+        multiply(Number(num1),Number(num2));
 
     }else{
-        const separated = numbers.split("÷");
-        const num1 = separated[0];
-        const num2 = separated[1];
-        divide(num1,num2);
+        const split = numbers.split("/");
+        const num1 = split[0];
+        const num2 = split[1];
+
+        if(Number(num2) === 0){
+            displayEvaluation("Cannot divide");
+            return;
+
+        }
+        divide(Number(num1),Number(num2));
     }
+
 }
 
 function add(num1,num2){
-    displayEvaluation(Number(num1) + Number(num2));
-    
-
+    currentValue = (num1 + num2).toFixed();
+    displayEvaluation(currentValue);
 }
+
 function divide(num1,num2){
-    displayEvaluation(Number(num1) - Number(num2));
-
+    currentValue = (num1 / num2).toFixed()
+    displayEvaluation(currentValue);
 }
+
 function multiply(num1,num2){
-    displayEvaluation(Number(num1) * Number(num2));
-
+    currentValue = (num1 * num2).toFixed()
+    displayEvaluation(currentValue);
 }
+
 function subtract(num1,num2){
-    displayEvaluation(Number(num1) - Number(num2));
+    currentValue = (num1 - num2).toFixed()
+    displayEvaluation(currentValue);
 
 }
 
 function displayEvaluation(result){
-    currentStatus.value = result;
+    const convert = String(result);
 
+    if(convert !== "NaN"){
+        currentStatus.value = result;
+
+    }
+    
+    
 }
